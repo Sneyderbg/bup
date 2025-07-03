f32f7ccaf99368cf2be63569bfeb70b79ba2dabe
sneyderbg <sneyder.buitrago@udea.edu.co>
2025-07-02 20:56:50 -0500

    ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 
-export default defineConfig({
+export default defineConfig((env) => ({
+  base: env.mode === "production" ? "/bup/" : "/",
   plugins: [solid(), tailwindcss()],
-});
+}));
